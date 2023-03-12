799bfa1d89ca7d7543788af56b59714dabddcd3b
Tagl <arnara14@ru.is>
2023-03-12 22:34:49 +0000

    icecreammachine random data

--- problems/icecreammachines/data/random_generator.py
+++ problems/icecreammachines/data/random_generator.py
@@ -3,9 +3,18 @@ import sys
 import random
 
 random.seed(int(sys.argv[-1]))
+min_n = eval(sys.argv[1])
+max_n = eval(sys.argv[2])
+min_m = eval(sys.argv[3])
+max_m = eval(sys.argv[4])
+min_k = eval(sys.argv[5])
+max_k = eval(sys.argv[6])
 
-a = random.randint(1, 100)
-b = random.randint(1, 100)
-c = random.randint(720, 1439)
+n = random.randint(min_n, max_n)
+m = random.randint(min_m, max_m)
+k = random.randint(min_k, max_k)
 
-sys.stdout.write("{}\n{}\n{}\n".format(a, b, c))
+choices = [random.randint(1, m) for _ in range(n)]
+
+sys.stdout.write("{} {} {}\n".format(n, m, k))
+sys.stdout.write("{}\n".format("\n".join(str(x) for x in choices)))
