6661aad9c394f4aa8d695410cc625d50e4c7e98b
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2023-03-15 23:28:50 +0100

    chatnoi: initial random data and naive submission

---
+++ problems/chatnoi/data/lib.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def tokens(n):
+  ts = set()
+  while len(ts) < n:
+    l = random.randint(1,5)
+    ts.add(''.join( random.choice(string.ascii_lowercase) for _ in range(l) ))
+  return list(ts)
+
