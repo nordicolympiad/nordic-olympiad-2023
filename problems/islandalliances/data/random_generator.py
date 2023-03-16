214b372927e76785e96a38cb4856af86a2e3aa04
Tagl <arnara14@ru.is>
2023-03-16 22:34:15 +0000

    Strengthen data

--- problems/islandalliances/data/random_generator.py
+++ problems/islandalliances/data/random_generator.py
@@ -93,12 +93,14 @@ min_m = eval(sys.argv[3])
 max_m = eval(sys.argv[4])
 min_q = eval(sys.argv[5])
 max_q = eval(sys.argv[6])
-gen_type = sys.argv[7]
+max_refuses = eval(sys.argv[7])
+gen_type = sys.argv[8]
 
-assert gen_type in ["any", "one_refuse", "cliques"]
+assert gen_type in ["any", "move_much_left", "move_much_right"]
 
 n = random.randint(min_n, max_n)
-max_m = min(max_m, n*(n-1)//2)
+max_m = min(max_m, n*(n-1)//2 - 1)
+min_m = min(min_m, max_m)
 m = random.randint(min_m, max_m)
 q = random.randint(min_q, max_q)
 
@@ -106,53 +108,78 @@ uf = UnionFind(n)
 U = {i for i in range(n)}
 edges = []
 queries = []
-accepts = 0
 refuses = 0
 
-for i in range(m):
-    a, b = uf.get_random_edge()
-    uf.add_edge(a, b)
-    edges.append((a, b))
+if gen_type.startswith("move_much"):
+    a, b = 0, 1
+    for _ in range(m):
+        if b < n:
+            uf.add_edge(a, b)
+            edges.append((a, b))
+        b += 1
+        if b >= n or b >= a + n//10:
+            a += 1
+            b = a+1
+else:
+    for i in range(m):
+        a, b = uf.get_random_edge()
+        uf.add_edge(a, b)
+        edges.append((a, b))
+m = len(edges)
 
 random.shuffle(edges)
 refuse_index = random.randint(0, q - 1)
 
+attempts = 0
+MAX_ATTEMPTS = 5*10**6
+
 if gen_type == "any":
-    for i in range(q):
-        if uf.size(0) == n:
-            q = len(queries)
-            break
-        a, b = uf.get_random_query()
-        if not uf.distrusts(a, b):
-            uf.unite(a, b)
-        else:
-            refuses += 1
-        queries.append((a, b))
-else:
     tree_edges = []
     tree_vis = set()
     ind = [i for i in range(n)]
     random.shuffle(ind)
     make_tree(tree_edges, tree_vis, ind, 0, n-1)
     random.shuffle(tree_edges)
-    for i in range(q):
-        a, b = tree_edges[i] if i < len(tree_edges) else uf.get_random_query()
+    while len(queries) < q and attempts < MAX_ATTEMPTS:
+        attempts += 1
+        a, b = tree_edges[attempts] if attempts < len(tree_edges) else uf.get_random_query()
         if not uf.distrusts(a, b):
             uf.unite(a, b)
             queries.append((a, b))
-        elif refuses < 1:
+        elif refuses < max_refuses:
             queries.append((a, b))
             refuses += 1
-    q = len(queries)
+elif gen_type.startswith("move_much"):
+    a, b = 0, max((n // 10, 1))
+    while len(queries) < q and attempts < MAX_ATTEMPTS:
+        attempts += 1
+        if a >= n-1:
+            break
+        if b < n and not uf.united(a, b):
+            if not uf.distrusts(a, b):
+                uf.unite(a, b)
+                queries.append((a, b) if gen_type == "move_much_left" else (b, a))
+            elif refuses < max_refuses:
+                queries.append((a, b) if gen_type == "move_much_left" else (b, a))
+                refuses += 1
+        b += 1
+        if b >= n:
+            a += 1
+            b = a + max((n//10, 1))
+
+q = len(queries)
 
     
-assert m == len(edges)
-assert q == len(queries)
+assert m == len(edges) > 0
+assert q == len(queries) > 0
 assert gen_type != "one_refuse" or refuses <= 1
+
+scramble = [i for i in range(n)]
+random.shuffle(scramble)
 
 sys.stdout.write("{} {} {}\n".format(n, m, q))
 for a, b in edges:
-    sys.stdout.write("{} {}\n".format(a+1, b+1))
+    sys.stdout.write("{} {}\n".format(scramble[a]+1, scramble[b]+1))
 for a, b in queries:
-    sys.stdout.write("{} {}\n".format(a+1, b+1))
+    sys.stdout.write("{} {}\n".format(scramble[a]+1, scramble[b]+1))
 
