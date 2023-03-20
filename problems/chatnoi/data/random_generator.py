ae9bd614ac873f9e1113b285471009ae39ba8bc3
Tagl <arnara14@ru.is>
2023-03-20 21:14:23 +0000

    chatnoi: big data

--- problems/chatnoi/data/random_generator.py
+++ problems/chatnoi/data/random_generator.py
@@ -9,6 +9,7 @@ t = eval(sys.argv[2])
 k = eval(sys.argv[3])
 q = eval(sys.argv[4])
 w = eval(sys.argv[5])
+max_m_i = eval(sys.argv[6])
 
 tokens = lib.tokens(t)
 data = [ random.choice(tokens) for _ in range(n)  ]
@@ -21,10 +22,10 @@ sys.stdout.write("{}\n".format(q))
 qs = []
 if random.randint(0,1) == 0:
   for i in range(q):
-    qs.append(w // q)
+    qs.append(min((w // q, max_m_i)))
 else:
   for i in range(q):
-    cur = random.randint(1, w - (q-i))
+    cur = random.randint(1, min((w - (q-i-1), max_m_i)))
     w -= cur
     qs.append(cur)
 
